b206a6183996a86be47b0d6c7eda9ee26dbf38d7
Yurito101 <yuritoyushino@gmail.com>
2024-07-21 17:14:31 +0700

    Add BadPacketsA.java

---
+++ src/main/java/me/yurito/anticheatbase/checks/impl/badpackets/BadPacketsA.java
@@ -0,0 +1,20 @@
+package me.yurito.anticheatbase.checks.impl.badpackets;
+
+import me.yurito.anticheatbase.checks.enums.CheckType;
+import me.yurito.anticheatbase.checks.types.Check;
+import me.yurito.anticheatbase.managers.profile.Profile;
+import me.yurito.anticheatbase.processors.Packet;
+
+public class BadPacketsA extends Check {
+    public BadPacketsA(Profile profile) {
+        super(profile, CheckType.BADPACKETS, "A", "Checks for BadPackets");
+    }
+
+    @Override
+    public void handle(Packet packet) {
+        final float pitch = profile.getRotationData().getPitch();
+        if (Math.abs(pitch) > 90F || Math.abs(pitch) < -90F) {
+            fail();
+        }
+    }
+}
