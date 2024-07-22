6ce55b103596fc9c04e299773c0c803b8ade6e7f
Yurito101 <yuritoyushino@gmail.com>
2024-07-22 15:07:25 +0700

    Update CheckHolder

--- src/main/java/me/yurito/anticheatbase/utils/custom/CheckHolder.java
+++ src/main/java/me/yurito/anticheatbase/utils/custom/CheckHolder.java
@@ -2,6 +2,7 @@ package me.yurito.anticheatbase.utils.custom;
 
 import me.yurito.anticheatbase.checks.annotations.Testing;
 import me.yurito.anticheatbase.checks.impl.aim.AimA;
+import me.yurito.anticheatbase.checks.impl.aim.AimB;
 import me.yurito.anticheatbase.checks.impl.aim.AimC;
 import me.yurito.anticheatbase.checks.impl.badpackets.BadPacketsA;
 import me.yurito.anticheatbase.checks.impl.speed.SpeedA;
@@ -41,6 +42,7 @@ public class CheckHolder {
                 AimAssist
                  */
                 new AimA(this.profile),
+                new AimB(this.profile),
                 new AimC(this.profile),
 
                 /*
